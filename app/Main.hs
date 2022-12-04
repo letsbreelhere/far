f784651b83b3d016b6829a289ec8410aaf1d1afe
letsbreelhere <letsbreelhere@gmail.com>
2022-12-04 10:47:48 -0800

    Cursor display

--- app/Main.hs
+++ app/Main.hs
@@ -1,29 +1,26 @@
 module Main (main) where
 
 import Brick
-import Brick.BChan (newBChan, writeBChan)
 import qualified Brick.Widgets.Border as B
-import qualified Brick.Widgets.Border.Style as BS
 import qualified Brick.Widgets.Center as C
-import qualified Graphics.Vty as V
 import System.Directory
-import Control.Monad.State
 
-type Name = ()
+data Name = FileBrowser | Preview
+  deriving (Show, Ord, Eq)
 type AppState = [String]
 type Event = ()
 
 drawUI :: AppState -> [Widget Name]
 drawUI s = (:[]) $ C.center $ vLimit 25 $ hLimit 140 $ B.border $ joinBorders $ filesPane s <+> previewPane
 
-filesPane :: AppState -> Widget n
-filesPane s = padTop Max . padBottom (Pad 1) . padRight (Pad 5) $ vBox (map str s) <=> str "Command"
+filesPane :: AppState -> Widget Name
+filesPane s = padTop Max . padRight (Pad 5) $ vBox (map str s) <=> showCursor FileBrowser (Location (0,0)) (str "Command")
 
 previewPane :: Widget n
 previewPane = B.border $ padLeft Max $ C.center $ str "Preview"
 
 ui :: App AppState Event Name
-ui = (simpleApp emptyWidget) { appDraw = drawUI }
+ui = (simpleApp emptyWidget) { appDraw = drawUI, appChooseCursor = showFirstCursor }
 
 main :: IO ()
 main = do
