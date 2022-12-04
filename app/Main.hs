6bcd58a1b4a11ba754eb7264e90c684004b8270e
letsbreelhere <letsbreelhere@gmail.com>
2022-12-04 00:55:23 -0800

    Some UI setup

--- app/Main.hs
+++ app/Main.hs
@@ -1,6 +1,32 @@
 module Main (main) where
 
-import Lib
+import Brick
+import Brick.BChan (newBChan, writeBChan)
+import qualified Brick.Widgets.Border as B
+import qualified Brick.Widgets.Border.Style as BS
+import qualified Brick.Widgets.Center as C
+import qualified Graphics.Vty as V
+import System.Directory
+import Control.Monad.State
+
+type Name = ()
+type AppState = [String]
+type Event = ()
+
+drawUI :: AppState -> [Widget Name]
+drawUI s = (:[]) $ C.center $ vLimit 25 $ hLimit 140 $ B.border $ joinBorders $ filesPane s <+> previewPane
+
+filesPane :: AppState -> Widget n
+filesPane s = padTop Max . padBottom (Pad 1) . padRight (Pad 5) $ vBox (map str s) <=> str "Command"
+
+previewPane :: Widget n
+previewPane = B.border $ padLeft Max $ C.center $ str "Preview"
+
+ui :: App AppState Event Name
+ui = (simpleApp emptyWidget) { appDraw = drawUI }
 
 main :: IO ()
-main = someFunc
+main = do
+  fs <- getCurrentDirectory >>= listDirectory
+  _ <- defaultMain ui fs
+  pure ()
