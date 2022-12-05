b9f21e3f4a8cf618e95e5ffcfa559a59953aacc3
letsbreelhere <letsbreelhere@gmail.com>
2022-12-05 12:20:06 -0800

    Type for rendering context

--- src/Rendering.hs
+++ src/Rendering.hs
@@ -5,18 +5,27 @@ import Types
 import Brick
 import qualified Brick.Widgets.Border as B
 import qualified Brick.Widgets.Center as C
-import Lens.Micro
 import qualified Brick.Widgets.List as List
 
-commandPane :: Widget Name
-commandPane =  showCursor Command (Location (0,0)) (str "Command")
+drawUI :: AppState -> [Widget Name]
+drawUI s = [withRenderCtx uiRoot s]
 
-filesPane :: AppState -> Widget Name
-filesPane s = List.renderList renderFile (s^.focus == FileBrowser) (s^.files)
+uiRoot :: RenderCtx (Widget Name)
+uiRoot = do
+  cp <- commandPane
+  fp <- filesPane
+  pp <- previewPane
+  pure $ C.center $ hLimit 140 $ B.border $ (fp <=> cp) <+> pp
+
+commandPane :: RenderCtx (Widget Name)
+commandPane = pure $ showCursor Command (Location (0,0)) (str "Command")
+
+filesPane :: RenderCtx (Widget Name)
+filesPane = do
+  fc <- viewing focus
+  fs <- viewing files
+  pure $ List.renderList renderFile (fc == FileBrowser) fs
   where renderFile selected (fname, _) = withAttr (attrName $ if selected then "selected" else "default") (str fname)
 
-previewPane :: Widget Name
-previewPane = B.border $ C.center $ showCursor Preview  (Location (0,0)) $ str "Preview"
-
-drawUI :: AppState -> [Widget Name]
-drawUI s = (:[]) $ C.center $ hLimit 140 $ B.border $ (filesPane s <=> commandPane) <+> previewPane
+previewPane :: RenderCtx (Widget Name)
+previewPane = pure $ B.border $ C.center $ showCursor Preview  (Location (0,0)) $ str "Preview"
--- src/Types.hs
+++ src/Types.hs
@@ -1,8 +1,12 @@
-{-# LANGUAGE TemplateHaskell #-}
+{-# LANGUAGE TemplateHaskell, GeneralizedNewtypeDeriving, RankNTypes #-}
+
 module Types (module Types) where
 
 import Lens.Micro.TH (makeLenses)
 import Brick.Widgets.List (List)
+import Control.Monad.Reader (Reader, MonadReader (ask), runReader)
+import Lens.Micro
+import Lens.Micro.Extras (view)
 
 data Name = FileBrowser | Preview | Command
   deriving (Show, Ord, Eq)
@@ -11,5 +15,17 @@ data AppState = AppState
   , _files :: List Name (String, String)
   }
 makeLenses ''AppState
+
+newtype RenderCtx a = RenderCtx { getRenderCtx :: Reader AppState a }
+  deriving (Functor, Applicative, Monad, MonadReader AppState)
+
+withRenderCtx :: RenderCtx a -> AppState -> a
+withRenderCtx = runReader . getRenderCtx
+
+getCtx :: RenderCtx AppState
+getCtx = RenderCtx ask
+
+viewing :: Lens' AppState a -> RenderCtx a
+viewing l = view l <$> getCtx
 
 type Event = ()
