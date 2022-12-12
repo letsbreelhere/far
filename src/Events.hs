9fddf3f3700a3cb48cf22723af34b5ca7cb6ffea
letsbreelhere <letsbreelhere@gmail.com>
2022-12-12 12:52:23 -0800

    Only find first match for updating matched files - much faster

--- src/Events.hs
+++ src/Events.hs
@@ -1,13 +1,14 @@
 module Events (handleEvent) where
 
+import Search (mkRegex)
 import Types
 import Util
 
 import Brick
 import Brick.Widgets.List (listSelectedL, listElementsL)
-import Search (findMatches, mkRegex)
 import Data.Foldable
 import Lens.Micro
+import qualified Text.Regex.PCRE as Regex
 import qualified Brick.Widgets.Edit as Edit
 import qualified Brick.Widgets.List as List
 import qualified Data.Text as Text
@@ -21,7 +22,7 @@ updateMatchedFiles = do
   grepRegex <- use (regexFrom . editorContentL)
   let mRegex = mkRegex $ Text.unpack grepRegex
   let matchedFiles' = case mRegex of
-        Just r -> Vec.filter (\(_, c) -> not . null $ findMatches r c) allFiles
+        Just r -> Vec.filter (\(_, c) -> not . null $ Regex.matchOnce r c) allFiles
         Nothing -> allFiles
   matchedFiles.listElementsL.= matchedFiles'
 
