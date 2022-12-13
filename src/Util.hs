bdc7eb2f6677ea7d128ef5a302bc9432d4aaa2ba
letsbreelhere <letsbreelhere@gmail.com>
2022-12-12 22:33:36 -0800

    Generalize equal setters

--- src/Util.hs
+++ src/Util.hs
@@ -17,27 +17,27 @@ import qualified Data.Text as Text
 filterMSeq :: (a -> IO Bool) -> Seq a -> IO (Seq a)
 filterMSeq p = foldM (\acc a -> p a >>= \b -> if b then pure (acc |> a) else pure acc) Seq.empty
 
-equalify :: MonadState s m => (ASetter' s a -> b -> s -> s) -> Lens' s a -> b -> m ()
+equalify :: MonadState s m => (ASetter' s a -> b -> s -> s) -> ASetter' s a -> b -> m ()
 equalify op l v = modify (op l v)
 
 infix 4 .=
-(.=) :: MonadState s m => Lens' s a -> a -> m ()
+(.=) :: MonadState s m => ASetter' s a -> a -> m ()
 (.=) = equalify (.~)
 
 infix 4 +=
-(+=) :: (MonadState s m, Num a) => Lens' s a -> a -> m ()
+(+=) :: (MonadState s m, Num a) => ASetter' s a -> a -> m ()
 (+=) = equalify (+~)
 
 infix 4 -=
-(-=) :: (MonadState s m, Num a) => Lens' s a -> a -> m ()
+(-=) :: (MonadState s m, Num a) => ASetter' s a -> a -> m ()
 (-=) = equalify (-~)
 
 infix 4 %=
-(%=) :: MonadState s m => Lens' s a -> (a -> a) -> m ()
+(%=) :: MonadState s m => ASetter' s a -> (a -> a) -> m ()
 (%=) = equalify (%~)
 
 infix 4 ?=
-(?=) :: MonadState s m => Lens' s (Maybe a) -> a -> m ()
+(?=) :: MonadState s m => ASetter' s (Maybe a) -> a -> m ()
 (?=) = equalify (?~)
 
 use :: MonadState s m => Getting a s a -> m a
